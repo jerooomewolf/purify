dccb717f1ef105fd87534c249a378e89fddbf149
Jerome <excore@outlook.com>
2020-12-30 15:54:06 -0500

    updates

--- src/HTMLPurifierDefinitionCacheFactory.php
+++ src/HTMLPurifierDefinitionCacheFactory.php
@@ -3,35 +3,14 @@
 namespace Stevebauman\Purify;
 
 use HTMLPurifier_DefinitionCacheFactory;
+use HTMLPurifier_DefinitionCache_Serializer;
+use HTMLPurifier_DefinitionCache_Null;
 
 /**
  * Responsible for creating definition caches.
  */
 class HTMLPurifierDefinitionCacheFactory extends HTMLPurifier_DefinitionCacheFactory
 {
-    /**
-     * @type array
-     */
-    protected $caches = array('Serializer' => array());
-
-    /**
-     * @type array
-     */
-    protected $implementations = array();
-
-    /**
-     * @type HTMLPurifier_DefinitionCache_Decorator[]
-     */
-    protected $decorators = array();
-
-    /**
-     * Initialize default decorators
-     */
-    public function setup()
-    {
-        $this->addDecorator('Cleanup');
-    }
-
     /**
      * Retrieves an instance of global definition cache factory.
      * @param HTMLPurifier_DefinitionCacheFactory $prototype
@@ -43,20 +22,10 @@ class HTMLPurifierDefinitionCacheFactory extends HTMLPurifier_DefinitionCacheFac
         if ($prototype !== null) {
             $instance = $prototype;
         } elseif ($instance === null || $prototype === true) {
-            $instance = new HTMLPurifier_DefinitionCacheFactory();
+            $instance = new HTMLPurifierDefinitionCacheFactory();
             $instance->setup();
         }
         return $instance;
-    }
-
-    /**
-     * Registers a new definition cache object
-     * @param string $short Short name of cache object, for reference
-     * @param string $long Full class name of cache object, for construction
-     */
-    public function register($short, $long)
-    {
-        $this->implementations[$short] = $long;
     }
 
     /**
@@ -93,19 +62,6 @@ class HTMLPurifierDefinitionCacheFactory extends HTMLPurifier_DefinitionCacheFac
         }
         $this->caches[$method][$type] = $cache;
         return $this->caches[$method][$type];
-    }
-
-    /**
-     * Registers a decorator to add to all new cache objects
-     * @param HTMLPurifier_DefinitionCache_Decorator|string $decorator An instance or the name of a decorator
-     */
-    public function addDecorator($decorator)
-    {
-        if (is_string($decorator)) {
-            $class = "HTMLPurifier_DefinitionCache_Decorator_$decorator";
-            $decorator = new $class;
-        }
-        $this->decorators[$decorator->name] = $decorator;
     }
 }
 
